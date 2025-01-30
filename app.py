eac0a521c7812419b55cf0edb6a9b4a6b324ed89
kevinseya <kevinseya@gmail.com>
2025-01-30 18:51:08 -0500

    style: :memo: Update README.md and message of print on page intitial.

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
+import os
 from flask import Flask, request, jsonify
 from model.model import Notificacion
 from repository.repository import save_notification
 from service.email_service import send_email
 from uuid import UUID
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+port = int(os.getenv("PORT", 5000))
+
+@app.route("/")
+def home():
+    return f"Webhook is running on port {port} 🚀"
 
 @app.route("/webhook_create_user", methods=["POST"])
 def webhook():
     """
-    Endpoint to recive notifications on webhook, and sent mail.
+    Endpoint to receive notifications on webhook and send email.
     """
     try:
         data = request.json
         data["user_id"] = UUID(data["user_id"])  
         data["date"] = datetime.utcnow()  
 
-        #Save on database
+        # Save on database
         notification = save_notification(data)
 
-        #Sent mail
+        # Send email
         email_sent = send_email(
             recipient=data["email"],
             subject="Notificación de Creación de Usuario",
@@ -28,12 +38,13 @@ def webhook():
         )
 
         if email_sent:
-            return jsonify({"message": "Notification recived and mail sent", "data": notification}), 201
+            return jsonify({"message": "Notification received and mail sent", "data": notification}), 201
         else:
-            return jsonify({"message": "Notification recived and fail mail sent", "data": notification}), 201
+            return jsonify({"message": "Notification received but mail failed to send", "data": notification}), 201
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    print(f"Webhook is running on port {port} 🚀") 
+    app.run(debug=True, host="0.0.0.0", port=port)
